59dced01629d5fce6ff555687b3d0d3c4664980f
Mojolaoluwa Fadahunsi <fadahunsi2018@yahoo.com>
2022-06-02 06:41:06 -0700

    python files

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Author- Mojolaoluwa Fadahunsi
+
+for number in range(0, 99):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
