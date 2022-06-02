0d57b913b70601a9e1f936d10d6dec7d44a0ada3
Mojolaoluwa Fadahunsi <fadahunsi2018@yahoo.com>
2022-06-02 06:30:18 -0700

    lowercase

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-#Author - Mojolaoluwa Fadahunsi
+# Author - Mojolaoluwa Fadahunsi
 
-for letter in range (97, 123):
+for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
