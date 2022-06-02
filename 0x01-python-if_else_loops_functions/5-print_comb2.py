31f075c9a3d87da4146dc3874ccc89772b926dba
Mojolaoluwa Fadahunsi <fadahunsi2018@yahoo.com>
2022-06-02 06:43:26 -0700

    updated

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # Author- Mojolaoluwa Fadahunsi
 
-for number in range(0, 99):
-    if number == 99:
+for number in range(0, 100):
+    if number == 100:
         print("{}".format(number))
     else:
         print("{:02}".format(number), end=", ")
